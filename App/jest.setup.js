e932ebe92a94b43aa5e50d5dd544769daaffaff7
bhuwanb23 <bhuwanseervi4567@gmail.com>
2026-07-10 16:40:41 +0530

    test: add mocks for expo-linear-gradient and prop-types in jest setup

--- App/jest.setup.js
+++ App/jest.setup.js
@@ -9,3 +9,30 @@ jest.mock('expo-constants', () => {
     default: mockConstants,
   };
 });
+
+jest.mock('expo-linear-gradient', () => {
+  const React = require('react');
+  return {
+    __esModule: true,
+    LinearGradient: React.forwardRef((props, ref) => {
+      const { children, ...rest } = props;
+      return React.createElement('View', { ...rest, ref }, children);
+    }),
+  };
+});
+
+jest.mock('prop-types', () => {
+  const noop = () => {};
+  return {
+    __esModule: true,
+    default: { string: noop, number: noop, func: noop, bool: noop, object: noop, array: noop, oneOfType: () => noop, oneOf: () => noop },
+    string: noop,
+    number: noop,
+    func: noop,
+    bool: noop,
+    object: noop,
+    array: noop,
+    oneOfType: () => noop,
+    oneOf: () => noop,
+  };
+});
