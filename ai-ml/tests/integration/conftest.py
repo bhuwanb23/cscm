9587ee1ccde32f29ee80bbbc2d7c30cd16e78dba
bhuwanb23 <bhuwanseervi4567@gmail.com>
2026-07-10 10:43:55 +0530

    fix(conftest): update sys.path to include legacy_models instead of models

--- ai-ml/tests/integration/conftest.py
+++ ai-ml/tests/integration/conftest.py
@@ -8,7 +8,7 @@ import pytest
 
 AI_ML_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
 sys.path.insert(0, AI_ML_ROOT)
-sys.path.insert(0, os.path.join(AI_ML_ROOT, 'models'))
+sys.path.insert(0, os.path.join(AI_ML_ROOT, 'legacy_models'))
 sys.path.insert(0, os.path.join(AI_ML_ROOT, 'api'))
 
 
