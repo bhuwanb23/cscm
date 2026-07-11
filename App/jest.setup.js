f12ab4cf4f7be902fc306e6f9d1ca74e3b80e0e1
bhuwanb23 <bhuwanseervi4567@gmail.com>
2026-07-11 14:48:22 +0530

    feat: mock @react-native-async-storage/async-storage for testing purposes

--- App/jest.setup.js
+++ App/jest.setup.js
@@ -36,3 +36,16 @@ jest.mock('prop-types', () => {
     oneOf: () => noop,
   };
 });
+
+jest.mock('@react-native-async-storage/async-storage', () => {
+  const store = {};
+  return {
+    __esModule: true,
+    default: {
+      getItem: jest.fn((key) => Promise.resolve(store[key] || null)),
+      setItem: jest.fn((key, value) => { store[key] = value; return Promise.resolve(); }),
+      removeItem: jest.fn((key) => { delete store[key]; return Promise.resolve(); }),
+      clear: jest.fn(() => { Object.keys(store).forEach(k => delete store[k]); return Promise.resolve(); }),
+    },
+  };
+});
